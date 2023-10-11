8e4150ef1dc6fc3f431441e344151a131034b0b3
Михаил Калмыков <68296867+Mike-Vazovskiy@users.noreply.github.com>
2023-10-11 14:05:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ let quoteAuthor = document.getElementById("author");
 async function getQuote(url) {
     const response = await fetch(url);
     let data = await response.json();
-    console.log(data)
     quote.innerHTML = data.content;
     quoteAuthor.innerHTML = data.author
 }
